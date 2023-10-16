9d725e1a7719c0bdfac91f5e3260d890b2248a86
Umar <syedu0324@gmail.com>
2023-10-15 21:11:08 -0400

    added variable

--- Pair.java
+++ Pair.java
@@ -1,7 +1,11 @@
 public class Pair {
     public void getNumberOfPairs() {
        System.out.println("Ibrahim is bad at ball");
-       String jjj = "ll";
+       boolean j = false;
+       char c = '8';
+       float f = 32.0f;
+       long l = 6400000000L;
+
     }
     // comment out
 }
